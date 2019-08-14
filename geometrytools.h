e84aaec89e7a62278390e1f4ef133024e6e21d74
Iftach Sella <iftach.sella@gmail.com>
2019-08-14 13:07:18 +0100

    initial matrix-based camera design (won't work)

--- geometrytools.h
+++ geometrytools.h
@@ -18,6 +18,44 @@ namespace geo {
 	std::array<double, 9> cofactorsPlus(std::array<double, 9> M);
 	std::array < double, 9 > invert3x3(std::array<double, 9> M);
 	std::array <double, 3> multiply3x3X3x1(std::array<double, 9> A, std::array<double, 3> B);
+	
+
+	template <class T> class Matrix2D {
+		//		|[0], [1]|		//
+		//		|[2], [3]|		//
+		std::array <T, 4> fields;
+		
+	public:
+		Matrix2D(T a, T b, T c, T d) {
+			this->fields = { a,b,c,d };
+		}
+
+		Matrix2D(T floats[4]) {
+			this->fields = { floats[0],floats[1],floats[2],floats[3] };
+		}
+
+		Matrix2D operator*(Matrix2D mat) {
+			float values[4];
+			values[0] = this->fields[0] * mat[0] + this->fields[1] * mat[2];
+			values[1] = this->fields[0] * mat[1] + this->fields[1] * mat[3];
+			values[2] = this->fields[2] * mat[0] + this->fields[3] * mat[2];
+			values[3] = this->fields[2] * mat[1] + this->fields[3] * mat[3];
+			return Matrix2D(values);
+		}
+
+		Matrix2D operator *= (Matrix2D mat) {
+			this->fields = (this->operator*(mat)).fields;
+		}
+
+		geo::vec operator * (geo::vec vector) {
+			return { this->fields[0] * vector.x + this->fields[1] * vector.y, this->fields[2] * vector.x + this->fields[3] * vector.y };
+		}
+
+		float& operator[] (size_t position) {
+			return this->fields[position];
+		}
+
+	};
 
 	//------------------------------------------UTILITY FUNCTIONS------------------------------------------
 
